be69ee4ecf66b4daf55b4f56e2d1f40e3060d2a1
Akshay <asj@vulcantechsoftware.com>
2016-06-25 13:58:28 +0530

    changes to support image search

--- py_bing_search/py_bing_search.py
+++ py_bing_search/py_bing_search.py
@@ -15,12 +15,15 @@ class PyBingSearch(object):
     QUERY_URL = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web' \
                  + '?Query={}&$top={}&$skip={}&$format={}&Adult=%27Off%27'
 
+    IMAGE_URL = 'https://api.datamarket.azure.com/Bing/Search/v1/Image' \
+                + '?Query={}&$top={}&$skip={}&$format={}&Adult=%27Off%27'
+
     def __init__(self, api_key, safe=False):
         self.api_key = api_key
         self.safe = safe
 
-    def search(self, query, limit=50, offset=0, format='json', timeout=5):
-        return self._search(query, limit, offset, format, timeout=timeout)
+    def search(self, query, limit=50, offset=0, format='json', timeout=5, source='Web'):
+        return self._search(query, limit, offset, format, timeout=timeout, source=source)
 
     def search_all(self, query, limit=50, format='json'):
         results = self._search(query, limit, 0, format)
@@ -30,11 +33,13 @@ class PyBingSearch(object):
             results += more_results
         return results
 
-    def _search(self, query, limit, offset, format, timeout=5):
+    def _search(self, query, limit, offset, format, timeout=5, source='Web'):
         '''
         Returns a list of result objects, with the url for the next page bing search url.
         '''
         url = self.QUERY_URL.format(urllib2.quote("'{}'".format(query)), limit, offset, format)
+        if source == "Image":
+            url = self.IMAGE_URL.format(urllib2.quote("'{}'".format(query)), limit, offset, format)
         r = requests.get(url, auth=("", self.api_key), timeout=timeout)
         try:
             json_results = r.json()
@@ -49,7 +54,7 @@ class PyBingSearch(object):
         except KeyError as kE:
             print "Couldn't extract next_link: KeyError: %s" % kE
             next_link = ''
-        return [Result(single_result_json) for single_result_json in json_results['d']['results']], next_link
+        return [Result(single_result_json, source) for single_result_json in json_results['d']['results']], next_link
 
 class Result(object):
     '''
@@ -75,10 +80,17 @@ class Result(object):
             self.type = meta['type']
             self.uri = meta['uri']
 
-    def __init__(self, result):
-        self.url = result['Url']
+    def __init__(self, result, source='Web'):
+        self.url = result.get('Url')
+        if source == 'Image':
+            self.url = result.get('MediaUrl')
         self.title = result['Title']
-        self.description = result['Description']
+        self.description = result.get('Description')
+        self.height = result.get('Height')
+        self.width = result.get('Width')
+        self.source = source
+        self.size = result.get('Size')
+        self.content_type = result.get('ContentType')
         self.id = result['ID']
 
         self.meta = self._Meta(result['__metadata'])
